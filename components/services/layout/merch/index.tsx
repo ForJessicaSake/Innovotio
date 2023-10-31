c1f7b0f4dd369a4b51c213b66357bf2d19c829d0
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-10-31 14:50:18 +0100

    fix: brands

--- components/services/layout/merch/index.tsx
+++ components/services/layout/merch/index.tsx
@@ -17,40 +17,27 @@ import Image from "next/image";
 
 const Merch = () => {
   const data = [
- 
     {
-      image: "../../../../assets/services/akranex.jpg",
+      image: "/../../../../assets/services/akranex.jpg",
     },
+
     {
-      image: "../../../../assets/services/bottle.jpg",
-    },
-    {
-      image: "../../../../assets/services/inn.jpg",
+      image: "/../../../../assets/services/bottle2.jpg",
     },
   ];
-  const mobileData = [
-    {
-      image: "../../../../assets/services/merch1.svg",
-    },
-    {
-      image: "../../../../assets/services/merch3.svg",
-    },
-    {
-      image: "../../../../assets/services/merch4.svg",
-    },
-    {
-      image: "../../../../assets/services/merch2.svg",
-    },
-  ];
+
   return (
     <section className="lg:pt-24 pt-14 text-text">
-      <div className="flex flex-col lg:space-x-5" id="merch">
+      <div
+        className="flex flex-col lg:flex-row justify-between"
+        id="merch"
+      >
         <div className=" flex flex-col">
           <h4 className="text-section font-medium">Merchandising</h4>
           <h1 className="pt-1 font-medium xl:text-4xl md:text-3xl text-2xl lg:max-w-lg">
             Seamless promotional merchandise for brands
           </h1>
-          <p className="pt-5 text-base text-justify text-texts lg:max-w-3xl">
+          <p className="pt-5 text-base text-justify text-texts lg:max-w-sm xl:max-w-lg">
             We specialize in creating and providing branded merchandise and
             promotional products for technology companies and their communities.
             Our primary focus is to design and produce merchandise that reflects
@@ -61,56 +48,40 @@ const Merch = () => {
             <Button className="my-5">Order Merchandise</Button>
           </Link>
         </div>
+          <Image
+            alt="merchandise"
+            width={590}
+            height={400}
+            src="/../../../../assets/services/akranex.jpg"
+            className="rounded-md"
 
-        <div className="flex flex-col py-10">
+          />
+
+        {/* <div className="flex flex-col justify-center items-center py-10">
           <Swiper
-            spaceBetween={20}
+            spaceBetween={10}
             centeredSlides={true}
             freeMode={true}
             navigation={true}
             modules={[FreeMode, Navigation]}
-            className="max-w-[100%] lg:max-w-[100%]"
+            className="max-w-[100%] lg:max-w-[100%] flex items-center justify-center h-full"
           >
             {data.map((client) => (
               <SwiperSlide key={client.image}>
-                <div className="md:mx-20 mx-7 xl:text-start text-center">
-                  <div className="md:flex hidden space-x-3 justify-center items-center">
+                <div className="mx-20">
+                  <div className="flex space-x-3 justify-center items-center ">
                     <img
+                      alt="merchandise"
+                    
                       src={client.image}
-                      className="rounded-md"
-                    />
-                    {/* <img
-                      src={client.image2}
-                      className="rounded-md xl:w-[650px] xl:h-[500px] lg:w-[500px] lg:h-[312px] md:w-[380px] md:h-[212px]"
-                    /> */}
-                  </div>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-
-          <Swiper
-            spaceBetween={20}
-            centeredSlides={true}
-            freeMode={true}
-            navigation={true}
-            modules={[FreeMode, Navigation]}
-            className="max-w-[100%] lg:max-w-[100%]"
-          >
-            {mobileData.map((client) => (
-              <SwiperSlide key={client.image}>
-                <div className="mx-10 xl:text-start text-center">
-                  <div className="md:hidden block">
-                    <img
-                      src={client.image}
-                      className="rounded-md w-[380px] h-[212px] "
+                      className="rounded-md min-w-[320px] min-h-[300px] sm:max-w-[65%] sm:max-h-[65%]"
                     />
                   </div>
                 </div>
               </SwiperSlide>
             ))}
           </Swiper>
-        </div>
+        </div> */}
       </div>
       <div className="lg:pt-10 pt-5 grid grid-cols-1 lg:grid-cols-3 gap-y-10 gap-x-5">
         <div>
